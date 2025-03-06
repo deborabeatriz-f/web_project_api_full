3a8f33ec845076b1e24c6fd3a4d49600b5292a07
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-06 16:49:27 -0300

    remove require from name, about and avatar

--- backend/models/user.js
+++ backend/models/user.js
@@ -14,14 +14,12 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     default: "Jacques Cousteau",
-    require: false,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
     default: "Explorer",
-    require: false,
     minlength: 2,
     maxlength: 30,
   },
@@ -29,7 +27,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     default:
       "https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg",
-    require: false,
     validator: {
       function(value) {
         return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(value);
