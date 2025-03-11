7e28e926b7f39ce6b750c9f0bfb86dc8592b5781
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-11 14:07:52 -0300

    update middleware auth js file

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const { JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
+  const authorization = req.headers.authorization;
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return res.status(403).send({ message: "Recurso negado" });
   }
