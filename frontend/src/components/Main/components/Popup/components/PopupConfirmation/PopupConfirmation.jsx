d3215ab0bbcdc1abc3adf0869ebf54ab41b0de20
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-12 14:34:01 -0300

    update popup confirmation jsx

--- frontend/src/components/Main/components/Popup/components/PopupConfirmation/PopupConfirmation.jsx
+++ frontend/src/components/Main/components/Popup/components/PopupConfirmation/PopupConfirmation.jsx
@@ -1,16 +1,21 @@
-function PopupConfirmation() {
+export default function PopupConfirmation({ closePopup, handleCardDelete }) {
   return (
     <div className="popup__container popup__container-deleteCard">
       <div className="popup__card">
         <h3 className="popup__title">Tem certeza?</h3>
-        <form className="popup__input input-deleteCard" novalidate>
-          <button type="submit" className="input__submit input__submit-delete">
+        <form className="popup__input input-deleteCard" noValidate>
+          <button
+            type="submit"
+            className="input__submit input__submit-delete"
+            onClick={handleCardDelete}
+          >
             Sim
           </button>
         </form>
         <button
           className="popup__button-close button-closeDelete"
           draggable="true"
+          onClick={closePopup}
         ></button>
       </div>
     </div>
