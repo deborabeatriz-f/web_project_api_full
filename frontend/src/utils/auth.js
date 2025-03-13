8e01b2575afb98c0eb6fdd58ecd4b3712ef6be0a
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-13 15:35:26 -0300

    edit base url from utils - auth js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://tripleten-around-full.onrender.com";
 
 export const signin = (email, password) => {
   return fetch(`${BASE_URL}/users/me`, {
